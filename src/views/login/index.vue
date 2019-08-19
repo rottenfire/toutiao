333f5b0ccb6385ec2b8c24bc9711fdeab6105189
rotten_fire <zhangjiaweia@126.com>
2019-08-19 19:08:41 +0800

    梳理一级路由结构

---
+++ src/views/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="login">
+    login
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+  #login {
+    width: 100%;
+    height: 100vh;
+    background-image: url(../../assets/img/login_bg.jpg);
+    background-size: cover;
+  }
+
+</style>
