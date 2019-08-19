6571bf8d11079dca3fd2a5ffa2d7f9065886f903
rotten_fire <zhangjiaweia@126.com>
2019-08-19 19:07:54 +0800

    梳理初始化页面结构

--- src/router.js
+++ src/router.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/home'
+import Login from './views/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '',
+      redirect: '/login'
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
